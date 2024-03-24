273facf5b626e615dbbde50733c56249b7497543
Okyere Adubofuor <oksberg89@gmail.com>
2024-03-23 21:05:22 -0700

    Configured using Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,5 +2,9 @@
 # Configured to use the private key ~/.ssh/school
 file {'/root/.ssh/config':
   ensure  => present,
-  content => 'Host 272031-web-01\n  Hostname 272031-web-01\nUser ubuntu\nIdentityFile ~/.ssh/school\nPasswordAuthentication no'
+  content => 'Host 272031-web-01\n\
+              Hostname 272031-web-01\n\
+              User ubuntu\n\
+              IdentityFile ~/.ssh/school\n\
+              PasswordAuthentication no\n'
 }
