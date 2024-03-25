32f9ad05281d330404eca53d8c66270ac0190ca3
Okyere Adubofuor <oksberg89@gmail.com>
2024-03-24 18:26:46 -0700

    Puppet ssh config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,17 @@
 # Creates a manifest called config in ~/.ssh/
 # Configured to use the private key ~/.ssh/school
-file {'/root/.ssh/school':
-  ensure  => present,
-  content => 'PasswordAuthentication => no\nIdentityFile /root/.ssh/school'
+include stdlib
+
+file_line {'No password authentication':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  replace => true
+}
+
+file_line {'Specifying identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  replace => true
 }
